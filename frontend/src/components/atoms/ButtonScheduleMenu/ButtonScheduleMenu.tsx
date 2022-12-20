49756fea75f2c8c61b1158096a7614f11357860b
Matheus Ferreira <ferreiromagrelo@gmail.com>
2022-12-20 15:52:49 -0300

    fix: disable daypicker clicking 2 times

--- frontend/src/components/atoms/ButtonScheduleMenu/ButtonScheduleMenu.tsx
+++ frontend/src/components/atoms/ButtonScheduleMenu/ButtonScheduleMenu.tsx
@@ -1,13 +1,29 @@
+import { Dispatch, SetStateAction } from 'react';
 import './styles.css';
 
 interface IProps {
     type: string;
     children: React.ReactNode;
+    index: number;
+    pressedFunction: Dispatch<SetStateAction<number>>;
+    buttonPressedId: number;
 }
 
-export function ButtonScheduleMenu({ children, type }: IProps) {
+export function ButtonScheduleMenu({
+    children,
+    type,
+    index,
+    pressedFunction,
+    buttonPressedId,
+}: IProps) {
     return (
-        <button className={'button-schedule-menu button-schedule-' + type}>
+        <button
+            className={'button-schedule-menu button-schedule-' + type}
+            onClick={() => {
+                if (buttonPressedId !== index) pressedFunction(index);
+                else pressedFunction(-1);
+            }}
+        >
             {children}
         </button>
     );
