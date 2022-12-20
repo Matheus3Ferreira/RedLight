a9e5becc92dc0222b2ca41616b0f14fb6dc00fa6
Matheus Ferreira <ferreiromagrelo@gmail.com>
2022-12-20 15:52:13 -0300

    fix: removing trash

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-import { HomepageContainer } from 'components/atoms/HomepageContainer/HomepageContainer';
-import { ScheduleMenu } from 'components/organims/ScheduleMenu/ScheduleMenu';
 import { Homepage } from 'pages/Homepage/Homepage';
-import { Header } from './components/organims/Header/Header';
 
 function App() {
     return (
